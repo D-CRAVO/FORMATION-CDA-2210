46cfb6989436b2a1b499582a15a0c310784b4d65
CRAVO David OFP <dcravo@arfp.asso.fr>
2022-11-29 17:00:08 +0100

    Create Exercice Objet

---
+++ Objet/Exercices/bouteille/bouteille/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Bottle
+{
+    private int capacity;
+    private string content;
+    private string material;
+
+    public abstract bool deboucher();
+    public abstract bool recycler();
+    public abstract bool remplir();
+}
+
