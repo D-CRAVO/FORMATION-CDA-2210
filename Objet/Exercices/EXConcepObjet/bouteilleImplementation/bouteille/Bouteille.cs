dc43b7380576e8db74b164ee8b205018d143342b
CRAVO David OFP <dcravo@arfp.asso.fr>
2022-12-01 17:01:00 +0100

    Création de l'implémenation EXConceptObjet Bouteille

---
+++ Objet/Exercices/EXConcepObjet/bouteilleImplementation/bouteille/Bouteille.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bouteille
+{
+    internal class Bouteille
+    {
+        private int contenanceEnCl;
+        private string contenuActuel;
+        private string matiere;
+        private int pourcentageDeRemplissage;
+        private bool bouteilleFermee;
+        private float quantitePresente;
+
+        public float vider(float quantite)
+        {
+            if (quantite > quantitePresente)
+            {
+                quantitePresente = 0;
+            }
+            else
+            {
+                quantitePresente = quantitePresente - quantite;
+            }
+            return quantitePresente;
+        }
+
+        public float remplir(float quantite)
+        {
+            if (quantitePresente + quantite > contenanceEnCl)
+            {
+                quantitePresente = contenanceEnCl;
+            }
+            else
+            {
+                quantitePresente = quantitePresente + quantite;
+            }
+            return quantitePresente;
+        }
+        public bool fermer()
+        {
+            if (bouteilleFermee == false)
+            {
+                bouteilleFermee = true;
+            }
+            else
+            {
+                Console.WriteLine("Bouteille déjà fermée");
+            }
+            return bouteilleFermee;
+        }
+
+        public bool ouvrir()
+        {
+            if (bouteilleFermee == true)
+            {
+                bouteilleFermee = false;
+            }
+            else
+            {
+                Console.WriteLine("Bouteille déjà ouverte");
+            }
+            return bouteilleFermee;
+        }
+    }
+}
+
--- Objet/Exercices/EXConcepObjet/bouteilleImplementation/bouteille/Program.cs
+++ Objet/Exercices/EXConcepObjet/bouteilleImplementation/bouteille/Program.cs
@@ -1,11 +1,4 @@
-﻿public class Bottle
+﻿public class Bouteille
 {
-    private int capacity;
-    private string content;
-    private string material;
-
-    public abstract bool deboucher();
-    public abstract bool recycler();
-    public abstract bool remplir();
-}
+    
 
