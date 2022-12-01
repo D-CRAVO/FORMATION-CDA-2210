019fe0806cc141747bfb14949ffbdc6616a7eeda
CRAVO David OFP <dcravo@arfp.asso.fr>
2022-12-01 16:55:03 +0100

    Création de l'implémentation C# de EXConceptObjet voiture

---
+++ Objet/Exercices/EXConcepObjet/voiture/voiture/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Voiture
+{
+    private String marque;
+    private String modele;
+    private String typeMoteur;
+    public void Avancer
+    {
+
+    }
+}
+
+public class Moteur
+{
+    private String carburant;
+    private float cylindree;
+    private int puissance;
+}
