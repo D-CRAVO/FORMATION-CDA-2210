c2b67308d20606d4ea2bc629733d450a96398940
CRAVO David OFP <dcravo@arfp.asso.fr>
2022-12-02 11:52:06 +0100

    Sauvegarde Jalon

---
+++ CRAVO David CDA2210/SuiteFibonacci/SuiteFibonacci/Program.cs
@@ -0,0 +1,43 @@
+﻿// Déclaration des variables
+
+int position = 2;
+string valeurRenseignee;
+int N;
+string resultat = "0\n1";
+uint nbPrecedent = 0;
+uint nbCourant = 1;
+uint nbSuivant;
+
+
+// Début du programme
+
+
+do
+{
+    Console.WriteLine("Combien de nombres de la suite de Fibonacci souhaitez-vous afficher ? : ");
+    valeurRenseignee = Console.ReadLine();
+    if (valeurRenseignee != "")
+    {
+        N = int.Parse(valeurRenseignee);
+        if (N > 2)
+        {
+            while (position < N)
+            {
+                nbSuivant = (nbPrecedent + nbCourant);
+                resultat = resultat + "\n" + nbSuivant;
+                nbPrecedent = nbCourant;
+                nbCourant = nbSuivant;
+                position++;
+            }
+        }
+        else
+        {
+            N = 2;
+        }
+        Console.WriteLine("Les " + N + " premiers nombres de la suite de Fibonacci sont :");
+        Console.WriteLine(resultat);
+    }
+    
+    
+} while (valeurRenseignee != "");
+Console.WriteLine("Programme terminé !");
