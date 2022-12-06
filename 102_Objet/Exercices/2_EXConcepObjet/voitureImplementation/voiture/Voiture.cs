acdc7fe9f80e3e6165a6adbc95a12f46e4e3598b
CRAVO David Logement <contact@davidcravo.fr>
2022-12-06 22:51:15 +0100

    Fin de l'ajout de commentaires

--- 102_Objet/Exercices/2_EXConcepObjet/voitureImplementation/voiture/Voiture.cs
+++ 102_Objet/Exercices/2_EXConcepObjet/voitureImplementation/voiture/Voiture.cs
@@ -1,22 +1,42 @@
+/// <summary>
+/// Classe Voiture
+/// </summary>
 public class Voiture
 {
-    /**
-     * Déclaration des attributs
-     */
+    /// <summary>
+    /// Marque de la voiture
+    /// </summary>
     private string marque;
+    /// <summary>
+    /// Modèle de la voiture
+    /// </summary>
     private string modele;
+    /// <summary>
+    /// Vitesse actuelle de la voiture en Kmh
+    /// </summary>
     private double vitesseKmh;
+    /// <summary>
+    /// Vitesse maximale de la voiture en Kmh
+    /// </summary>    
     private double vMaxKmh;
+    /// <summary>
+    /// Moteur de la voiture
+    /// </summary>
     private Moteur moteur;
+    /// <summary>
+    /// Roue de la voiture
+    /// </summary>
     private Roue roue;
 
-
+    /// <summary>
+    /// Accesseurs du moteur de la voiture
+    /// </summary>
     public Moteur Moteur { get { return moteur; } set { moteur = value; } }
 
 
-    /**
-     * Constructeur à vide
-     */
+    /// <summary>
+    /// Constructeur à vide
+    /// </summary>
     public Voiture()
     {
         marque = "Seat";
@@ -27,10 +47,25 @@ public class Voiture
         roue = new Roue();
     }
 
- 
-    /**
-     * Constructeur classique
-     */
+
+    /// <summary>
+    /// Constructeur classique
+    /// </summary>
+    /// <param name="_marque">Marque de la voiture</param>
+    /// <param name="_modele">Modèle de la voiture</param>
+    /// <param name="_vitesseKmh">Vitesse actuelle de la voiture</param>
+    /// <param name="_vMaxKmh">Vitesse maximale de la voiture</param>
+    /// <param name="_carburant">Type de carburant du moteur</param>
+    /// <param name="_cylindree">Cylindrée du moteur</param>
+    /// <param name="_puissance">Puissance du moteur</param>
+    /// <param name="_moteurTourne">Indique si le moteur tourne</param>
+    /// <param name="_poidsEquilibrage">Poids d'équilibrage de la roue</param>
+    /// <param name="_largeur">Largeur du pneu</param>
+    /// <param name="_hauteur">Hauteur du pneu</param>
+    /// <param name="_pression">Pression du pneu</param>
+    /// <param name="_matiere">Matière de la jante</param>
+    /// <param name="_couleur">Couleur de la jante</param>
+    /// <param name="_rayonEnPouces">Rayon de la jante en Pouces</param>
     public Voiture(string _marque, string _modele, double _vitesseKmh, double _vMaxKmh,
         string _carburant, double _cylindree, uint _puissance, bool _moteurTourne,
         uint _poidsEquilibrage, 
@@ -46,9 +81,13 @@ public class Voiture
     }
 
 
-    /**
-     * Faire accélérer la voiture
-     */
+    /// <summary>
+    /// Faire accélérer la voiture
+    /// </summary>
+    /// <returns>
+    /// "true" si la voiture peut accélérer
+    /// "false" dans le cas contraire
+    /// </returns>
     public bool Accelerer() 
     {
         if (Moteur.MoteurTourne)
@@ -63,9 +102,13 @@ public class Voiture
     }
 
 
-    /**
-     * Faire ralentir la voiture
-     */
+    /// <summary>
+    /// Faire ralentir la voiture
+    /// </summary>
+    /// <returns>
+    /// "true" si la voiture peut ralentir
+    /// "false dans le cas contraire
+    /// </returns>
     public bool Ralentir() 
     {
         if (Moteur.MoteurTourne)
