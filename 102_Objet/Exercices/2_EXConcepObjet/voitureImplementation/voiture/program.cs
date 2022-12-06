7e0f45be0ad3dd38ec713ce4c777d90b7cddb384
CRAVO David Logement <contact@davidcravo.fr>
2022-12-06 23:17:10 +0100

    Tests

--- 102_Objet/Exercices/2_EXConcepObjet/voitureImplementation/voiture/program.cs
+++ 102_Objet/Exercices/2_EXConcepObjet/voitureImplementation/voiture/program.cs
@@ -10,6 +10,17 @@ namespace voiture
     {
         static void Main(string[] args)
         {
+            Voiture cordoba = new Voiture();
+            if (!cordoba.Moteur.MoteurTourne)
+            {
+                bool resultat00 = cordoba.Moteur.Demarrer();
+                if (resultat00)
+                {
+                    bool resultat01 = cordoba.Accelerer();
+                }
+                bool resultat02 = cordoba.Moteur.Arreter();
+            }
+
             //Voiture(marque,modele,vitesseKmh,vMaxKmh,carburant,cylindree,puissance,moteurTourne,poidsEquilibrage,largeur,hauteur,pression,matiere,couleur,rayonEnPouces)
             Voiture leon = new Voiture("Seat", "Leon", 200, 200, "diesel", 2.0, 184, false, 0, 205, 55, 1.9f, "aluminium", "gris", 17);
 
@@ -18,7 +29,7 @@ namespace voiture
                 bool resultat03 = leon.Moteur.Demarrer();
                 if (resultat03)
                 {
-                    bool resultat04 = leon.Accelerer();
+                    bool resultat04 = leon.Ralentir();
                 }
             }
             
