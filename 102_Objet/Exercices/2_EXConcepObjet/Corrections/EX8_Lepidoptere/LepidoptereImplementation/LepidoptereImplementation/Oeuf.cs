50642b0bc22993b6d390f57c53d2c7f197db93bf
CRAVO David Logement <contact@davidcravo.fr>
2022-12-12 13:43:35 +0100

    Relecture exercice

---
+++ 102_Objet/Exercices/2_EXConcepObjet/Corrections/EX8_Lepidoptere/LepidoptereImplementation/LepidoptereImplementation/Oeuf.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LepidoptereImplementation
+{
+    internal class Oeuf : StadeEvolution
+    {
+        Oeuf() { }
+
+        public override string SeDeplacer() 
+        {
+            return "essai";
+        }
+
+        public override StadeEvolution SeMetamorphoser() 
+        {
+            return new Chenille();
+        }
+
+
+    }
+
+
+}
+
+    
---
+++ 102_Objet/Exercices/2_EXConcepObjet/Corrections/EX8_Lepidoptere/LepidoptereImplementation/LepidoptereImplementation/StadeEvolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LepidoptereImplementation
+{
+    public abstract class StadeEvolution
+    {
+        StadeEvolution() 
+        {
+
+        }
+
+        public abstract string SeDeplacer();
+
+        public abstract StadeEvolution SeMetamorphoser();
+    }
+
+    
+    
+}
