ecfa59b576cf667bd90bd3e4c675bf5fa20cec89
CRAVO David OFP <dcravo@arfp.asso.fr>
2022-12-15 12:56:11 +0100

    Sauvegarde midi

--- 102_Objet/Exercices/2_EXConcepObjet/Revision/VoitureRevision/VoitureRevision/Moteur.cs
+++ 102_Objet/Exercices/2_EXConcepObjet/Revision/VoitureRevision/VoitureRevision/Moteur.cs
@@ -9,5 +9,44 @@ namespace VoitureRevision
     internal class Moteur
     {
         private bool enMarche;
+
+        public Moteur (bool _enMarche)
+        {
+            enMarche = _enMarche;
+        }
+
+        public Moteur() : this(false){ }
+
+        public Moteur(Moteur _moteurCopie) : this 
+            (
+                _moteurCopie.enMarche
+            )
+        {
+        }
+
+        public bool Demarrer()
+        {
+            if (!enMarche)
+            {
+                enMarche = true;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Eteindre()
+        {
+            if (enMarche)
+            {
+                enMarche = false;
+                return true;
+            }
+            return false;
+        }
+
+        public bool EntrainerRoue(Roue _roueAvg, Roue _roueAvd)
+        {
+            return enMarche && _roueAvg.Tourner() && _roueAvd.Tourner();
+        }
     }
 }
--- 102_Objet/Exercices/2_EXConcepObjet/Revision/VoitureRevision/VoitureRevision/program.cs
+++ 102_Objet/Exercices/2_EXConcepObjet/Revision/VoitureRevision/VoitureRevision/program.cs
@@ -11,11 +11,28 @@ namespace VoitureRevision
         static void Main(string[] args)
         {
             Roue roueAvd = new Roue(false);
-            Roue roueAvg = new Roue();
+            Roue roueAvg = new Roue(false);
+            Roue roueAvdDefaut = new Roue();
+            Roue roueAvgDefaut = new Roue();
+            Roue RoueAvdCopie = new Roue(roueAvdDefaut);
+            Roue roueAvgCopie = new Roue(roueAvgDefaut);
             bool result01 = roueAvd.Tourner();
             bool result02 = roueAvd.Tourner();
             bool result03 = roueAvd.Arreter();
             bool result04 = roueAvd.Arreter();
+
+            Moteur moteurClassique = new Moteur(false);
+            Moteur moteurDefaut = new Moteur();
+            Moteur moteurCopie = new Moteur(moteurDefaut);
+            bool result05 = moteurClassique.Demarrer();
+            bool result06 = moteurClassique.Demarrer();
+            bool result07 = moteurClassique.Eteindre();
+            bool result08 = moteurClassique.Eteindre();
+            bool result09 = moteurClassique.Demarrer();
+            bool result10 = moteurClassique.EntrainerRoue(roueAvg, roueAvd);
+            bool result11 = moteurClassique.EntrainerRoue(roueAvg, roueAvg);
+
+            Voiture voitureClassique = new Voiture("Seat", false, false);
         }
     }
 }
