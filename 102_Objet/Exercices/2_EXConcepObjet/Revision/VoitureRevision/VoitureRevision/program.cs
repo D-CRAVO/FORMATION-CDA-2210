bc4754ffa3305bb94bd521a6e8e29bd154e7303c
CRAVO David Logement <contact@davidcravo.fr>
2022-12-15 06:49:03 +0100

    Création exercice VoitureRevision

---
+++ 102_Objet/Exercices/2_EXConcepObjet/Revision/VoitureRevision/VoitureRevision/program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoitureRevision
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Roue roueAvd = new Roue(false);
+            Roue roueAvg = new Roue();
+            bool result01 = roueAvd.Tourner();
+            bool result02 = roueAvd.Tourner();
+            bool result03 = roueAvd.Arreter();
+            bool result04 = roueAvd.Arreter();
+        }
+    }
+}
