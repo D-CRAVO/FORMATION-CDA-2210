fe7b0673256200b7f38ce78bdac67d5e0319725c
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-01-09 16:49:42 +0100

    Sauvegarde rapide Après-Midi

---
+++ 102_Objet/Exercices/3_EXProgrOrienteObjet/EX1_Compte/CompteCode/AppCompte/Program.cs
@@ -0,0 +1,41 @@
+﻿using LibraryCompte;
+
+namespace AppCompte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Création des comptes bancaires */
+            Compte david = new Compte("David", 0, -250);
+            Compte john = new Compte("John", 0, -500);
+            Compte jane = new Compte();
+
+            // Console.WriteLine(david);
+
+            /* Opérations sur les comptes */
+            try
+            {
+                david.Crediter(500);
+                string informationsCompte = david.ToString();
+
+                bool debiterTrue = david.Debiter(100); // true
+                // bool debiterFalse = david.Debiter(5000); // exception
+
+                bool transfererTrue = david.TransfererVers(john, 60); // true
+                // bool transfererFalse = david.TransfererVers(john, 5000); // exception
+
+
+                string result4 = jane.Comparer(john);
+                string result5 = david.Comparer(john);
+                bool superieurTrue = david.SuperieurA(john); // true
+                bool superieurFalse = jane.SuperieurA(david); // false
+
+            }
+            catch (Exception e)
+            {
+                string result = e.Message;
+            }
+        }
+    }
+}
