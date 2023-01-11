6982cf1c007c1931b56a8db3f7d688f05b69e6cc
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-01-11 12:46:38 +0100

    Sauvegarde rapide Matin

--- 102_Objet/Exercices/3_EXProgrOrienteObjet/EX5_421/421Code/Library421/De.cs
+++ 102_Objet/Exercices/3_EXProgrOrienteObjet/EX5_421/421Code/Library421/De.cs
@@ -1,14 +1,17 @@
-﻿namespace Library421
+﻿
+
+namespace Library421
 {
     public class De
     {
         private int valeur;
+        private static int valeurMax = 6;
 
         public int Valeur { get { return valeur; } }
 
         public De ()
         {
-           valeur = new Aleatoire();
+           valeur = Aleatoire.Instance().Nouveau(1, valeurMax);
         }
 
        
