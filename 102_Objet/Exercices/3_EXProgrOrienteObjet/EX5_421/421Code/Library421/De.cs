88f2ccc0a83cdeb490b9c8d073ea247656d6d39a
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-01-11 17:00:16 +0100

    Ajout constructeur classique

--- 102_Objet/Exercices/3_EXProgrOrienteObjet/EX5_421/421Code/Library421/De.cs
+++ 102_Objet/Exercices/3_EXProgrOrienteObjet/EX5_421/421Code/Library421/De.cs
@@ -5,15 +5,22 @@ namespace Library421
     public class De
     {
         private int valeur;
-        private static int valeurMax = 6;
+        private static int valeurMin;
+        private static int valeurMax;
 
         public int Valeur { get { return valeur; } }
 
         public De ()
         {
-           valeur = Aleatoire.Instance().Nouveau(1, valeurMax);
+            valeurMin = 1;
+            valeurMax = 6;
+            valeur = Aleatoire.Instance().Nouveau(valeurMin, valeurMax);
         }
 
+        public De (int _valeurMin, int _valeurMax)
+        {
+            valeur = Aleatoire.Instance().Nouveau(valeurMin, valeurMax);
+        }
        
     }
 }
