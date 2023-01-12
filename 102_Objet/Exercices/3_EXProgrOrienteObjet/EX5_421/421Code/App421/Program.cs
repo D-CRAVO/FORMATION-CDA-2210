e1d3ad9b055b250214a21f232a88d0f6f5afdd2f
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-01-12 12:59:57 +0100

    Sauvegarde rapide Matin

---
+++ 102_Objet/Exercices/3_EXProgrOrienteObjet/EX5_421/421Code/App421/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Library421;
+using System.Security.Cryptography;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App421
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            De de1 = new De();
+            De de2 = new De();
+            De de3 = new De();
+            int result01 = de1.CompareTo(de2);
+            triDe(de1, de2, de3);
+        }
+    }
+}
--- 102_Objet/Exercices/3_EXProgrOrienteObjet/EX5_421/421Code/Library421/De.cs
+++ 102_Objet/Exercices/3_EXProgrOrienteObjet/EX5_421/421Code/Library421/De.cs
@@ -2,7 +2,7 @@
 
 namespace Library421
 {
-    public class De
+    public class De : IComparable<De>
     {
         private int valeur;
         private static int valeurMin;
@@ -21,6 +21,87 @@ namespace Library421
         {
             valeur = Aleatoire.Instance().Nouveau(valeurMin, valeurMax);
         }
-       
+
+        public int CompareTo(De? other)
+        {
+            if (other == null)
+            {
+                throw new Exception("Veuillez lancer le dé");
+            }
+            else if (this.Valeur < other.Valeur)
+            {
+                return -1;
+            }
+            else if (this.Valeur > other.Valeur) 
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int Rouler()
+        {
+            return Aleatoire.Instance().Nouveau(valeurMin, valeurMax);
+        }
+
+        public void triDe(De _de1, De _de2, De _de3)
+        {
+            De temp;
+            if (_de1 == null || _de2 == null || _de3 == null)
+            {
+                throw new Exception("Veuillez lancer tous les dés");
+            }
+            if (_de1.CompareTo(_de2) == -1)
+            {
+                if (_de2.CompareTo(_de3) == -1)
+                {
+                    temp = _de1;
+                    _de1 = _de3;
+                    _de3 = temp;
+                }
+                else
+                {
+                    if (_de3.CompareTo(_de1) == -1)
+                    {
+                        temp = _de1;
+                        _de1 = _de2;
+                        _de2 = temp;
+                    }
+                    else
+                    {
+                        temp = _de1;
+                        _de1 = _de2;
+                        _de2 = _de3;
+                        _de3 = temp;
+                    }
+                }
+            }
+            else 
+            {
+                if (_de2.CompareTo(_de3) == -1)
+                {
+                    if (_de1.CompareTo(_de3) == -1)
+                    {
+                        temp = _de1;
+                        _de1 = _de3;
+                        _de3 = _de2;
+                        _de2 = temp;
+                    }
+                    else
+                    {
+                        temp = _de2;
+                        _de2 = _de3;
+                        _de3 = temp;
+
+                    }
+                }
+            }
+        }
+
+
+
     }
 }
