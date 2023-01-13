e4aad69766b24620dbcc6dee347eb56e145c8ddd
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-01-13 08:15:55 +0100

    Création Copie de travail exercice 421Code

---
+++ 102_Objet/Exercices/3_EXProgrOrienteObjet/EX5_421/421Code - Copie/App421/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using Library421;
+using System.Security.Cryptography;
+using System;
+
+
+namespace App421
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            De de1 = new De();
+            De de2 = new De();
+            De de3 = new De();
+            int result01 = de1.CompareTo(de2);
+            TriDe1(de1, de2, de3);
+            
+        }
+    }
+}
---
+++ 102_Objet/Exercices/3_EXProgrOrienteObjet/EX5_421/421Code - Copie/Library421/Aleatoire.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library421
+{
+    public class Aleatoire : Random
+    {
+        private static Aleatoire monAleatoire = null;
+
+        private Aleatoire()
+        {
+        }
+
+        public static Aleatoire Instance()
+        {
+            if (monAleatoire == null)
+            {
+                monAleatoire = new Aleatoire();
+            }
+            return monAleatoire;
+        }
+
+        public int Nouveau(int valMin, int valMax)
+        {
+            return base.Next(valMin, valMax+1);
+        }
+    }
+}
