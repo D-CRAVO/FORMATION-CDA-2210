fd512e156da8fd7e0729276b63bff614100a2c82
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-01-18 08:06:47 +0100

    Suppression dossier

---
+++ 102_Objet/Exercices/3_EXProgrOrienteObjet/EXA_Exception/ExempleException/ExempleException/Exceptions/DivideByZeroExemple.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExempleException.Exceptions
+{
+    internal class DivideByZeroExemple : Exception
+    {
+        public DivideByZeroExemple() { }
+    }
+}
---
+++ 102_Objet/Exercices/3_EXProgrOrienteObjet/EXA_Exception/ExempleException/ExempleException/Fractions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExempleException
+{
+    internal class Fractions
+    {
+        int numerateur;
+        int denominateur;
+
+        public Fractions(int _numerateur, int _denominateur)
+        {
+            numerateur= _numerateur;
+            denominateur= _denominateur;    
+        }
+
+        public Fractions(int _numerateur) : this (_numerateur, 1) { }
+    }
+}
