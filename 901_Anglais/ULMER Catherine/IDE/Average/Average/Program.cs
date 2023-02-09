8080aa495e1dcc99c64e725c65350dbaa5d860e3
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-02-09 17:00:37 +0100

    Sauvegarde rapide du soir

---
+++ 901_Anglais/ULMER Catherine/IDE/Average/Average/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Average
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the first number : ");
+            float number1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the second number : ");
+            float number2 = float.Parse(Console.ReadLine());
+
+            float average = (number1 + number2) / 2;
+            Console.WriteLine("The average of " + number1 + " and " + 
+                number2 + " is : " + average);
+        }
+    }
+}
