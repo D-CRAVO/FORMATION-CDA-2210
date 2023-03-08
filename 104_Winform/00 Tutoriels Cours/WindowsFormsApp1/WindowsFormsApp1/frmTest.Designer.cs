1befd7831db277ec95206e67df1c2ae78e0caa1a
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-03-08 16:46:14 +0100

    Relecture 421

---
+++ 104_Winform/00 Tutoriels Cours/WindowsFormsApp1/WindowsFormsApp1/frmTest.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQuitter = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.txtCible = new System.Windows.Forms.TextBox();
+            this.btnCopier = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnQuitter
+            // 
+            this.btnQuitter.Location = new System.Drawing.Point(204, 223);
+            this.btnQuitter.Name = "btnQuitter";
+            this.btnQuitter.Size = new System.Drawing.Size(75, 23);
+            this.btnQuitter.TabIndex = 0;
+            this.btnQuitter.Text = "Quitter";
+            this.btnQuitter.UseVisualStyleBackColor = true;
+            this.btnQuitter.Click += new System.EventHandler(this.btnQuitter_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cible";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(204, 100);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(100, 20);
+            this.txtSource.TabIndex = 3;
+            // 
+            // txtCible
+            // 
+            this.txtCible.Location = new System.Drawing.Point(204, 146);
+            this.txtCible.Name = "txtCible";
+            this.txtCible.Size = new System.Drawing.Size(100, 20);
+            this.txtCible.TabIndex = 4;
+            // 
+            // btnCopier
+            // 
+            this.btnCopier.Location = new System.Drawing.Point(204, 194);
+            this.btnCopier.Name = "btnCopier";
+            this.btnCopier.Size = new System.Drawing.Size(75, 23);
+            this.btnCopier.TabIndex = 5;
+            this.btnCopier.Text = "Copier";
+            this.btnCopier.UseVisualStyleBackColor = true;
+            this.btnCopier.Click += new System.EventHandler(this.btnCopier_Click);
+            // 
+            // frmTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCopier);
+            this.Controls.Add(this.txtCible);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnQuitter);
+            this.Name = "frmTest";
+            this.Text = "frmTest";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQuitter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.TextBox txtCible;
+        private System.Windows.Forms.Button btnCopier;
+    }
+}
---
+++ 104_Winform/00 Tutoriels Cours/WindowsFormsApp1/WindowsFormsApp1/frmTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmTest : Form
+    {
+        public frmTest()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopier_Click(object sender, EventArgs e)
+        {
+            // recopie de la valeur saisie de la source vers la cible
+            txtCible.Text = txtCible.Text + " " + txtSource.Text;
+            // efface la source : on utilise la méthode Clear
+            txtSource.Clear();
+            // positionne le focus : on utilise la méthode Focus
+            txtSource.Focus();
+        }
+
+        private void btnQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+       
+    }
+}
