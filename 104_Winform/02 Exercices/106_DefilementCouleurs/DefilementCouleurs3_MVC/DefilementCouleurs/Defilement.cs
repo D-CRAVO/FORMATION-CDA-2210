5253689e0938e088c916302fec5979c091bd402e
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-03-17 11:51:58 +0100

    Update DefilementCouleurs3

--- 104_Winform/02 Exercices/106_DefilementCouleurs/DefilementCouleurs3_MVC/DefilementCouleurs/Defilement.cs
+++ 104_Winform/02 Exercices/106_DefilementCouleurs/DefilementCouleurs3_MVC/DefilementCouleurs/Defilement.cs
@@ -40,51 +40,48 @@ namespace DefilementCouleurs
 
         private void numericUpDownRouge_ValueChanged(object sender, EventArgs e)
         {
-            hScrollBarRouge.Value = int.Parse(numericUpDownRouge.Text);
             maCouleur = Color.FromArgb(int.Parse(numericUpDownRouge.Text), maCouleur.G,maCouleur.B);
             mettreAJourIHM(maCouleur);
         }
 
         private void numericUpDownVert_ValueChanged(object sender, EventArgs e)
         {
-            hScrollBarVert.Value = int.Parse(numericUpDownVert.Text);
             maCouleur = Color.FromArgb(maCouleur.R, int.Parse(numericUpDownVert.Text), maCouleur.B);
             mettreAJourIHM(maCouleur);
         }
 
         private void numericUpDownBleu_ValueChanged(object sender, EventArgs e)
         {
-            hScrollBarBleu.Value = int.Parse(numericUpDownBleu.Text);
             maCouleur = Color.FromArgb(maCouleur.R, maCouleur.G, int.Parse(numericUpDownBleu.Text));
             mettreAJourIHM(maCouleur);
         }
 
         private void hScrollBarRouge_Scroll(object sender, ScrollEventArgs e)
         {
-            numericUpDownRouge.Text = hScrollBarRouge.Value.ToString();
             maCouleur = Color.FromArgb(hScrollBarRouge.Value, maCouleur.G, maCouleur.B);
             mettreAJourIHM(maCouleur);
         }
 
         private void hScrollBarVert_Scroll(object sender, ScrollEventArgs e)
         {
-            numericUpDownVert.Text = hScrollBarVert.Value.ToString();
             maCouleur = Color.FromArgb(maCouleur.R, hScrollBarVert.Value, maCouleur.B);
             mettreAJourIHM(maCouleur);
         }
 
         private void hScrollBarBleu_Scroll(object sender, ScrollEventArgs e)
         {
-            numericUpDownBleu.Text = hScrollBarBleu.Value.ToString();
             maCouleur = Color.FromArgb(maCouleur.R, maCouleur.G, hScrollBarBleu.Value);
             mettreAJourIHM(maCouleur);
         }
 
         private void mettreAJourIHM(Color _couleur)
         {
-            //numericUpDownRouge.Text = maCouleur.R.ToString();
-            //numericUpDownVert.Text = maCouleur.G.ToString();
-            //numericUpDownBleu.Text = maCouleur.B.ToString();
+            hScrollBarRouge.Value = int.Parse(_couleur.R.ToString());
+            hScrollBarVert.Value = int.Parse(_couleur.G.ToString());
+            hScrollBarBleu.Value = int.Parse(_couleur.B.ToString());
+            numericUpDownRouge.Text = _couleur.R.ToString();
+            numericUpDownVert.Text = _couleur.G.ToString();
+            numericUpDownBleu.Text = _couleur.B.ToString();
             textBoxCouleur.BackColor = Color.FromArgb((int)_couleur.R, (int)_couleur.G, (int)_couleur.B);
         }
     }
