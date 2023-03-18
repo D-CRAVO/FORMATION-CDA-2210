8ae438df7b778ba472993acfa4eb67aa9d863c24
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-03-18 07:16:59 +0100

    Create Emprunts7

---
+++ 104_Winform/02 Exercices/107_Emprunts/Emprunts7/ClassLibraryCalculs/Calculs.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryCalculs
+{
+    public class Calculs
+    {
+        public static int calculNbRemboursements(string _periodicite, int _nbMensualites)
+        {
+            if (_periodicite == "Mensuelle")
+            {
+                return _nbMensualites;
+            }
+            else if (_periodicite == "Bimestrielle")
+            {
+                return _nbMensualites / 2;
+            }
+            else if (_periodicite == "Trimestrielle")
+            {
+                return _nbMensualites / 3;
+            }
+            else if (_periodicite == "Semestrielle")
+            {
+                return _nbMensualites / 6;
+            }
+            else
+            {
+                return _nbMensualites / 12;
+            }
+        }
+
+        public static double calculRemboursements(double _capitalEmprunte, double _tauxCalcul, int _nbMensualites)
+        {
+            return _capitalEmprunte * (_tauxCalcul / (1 - Math.Pow((1 + _tauxCalcul), -_nbMensualites)));
+        }
+
+        public static double calculTauxRemboursements(double _tauxAnnuel, string _periodicite)
+        {
+            if (_periodicite == "Mensuelle")
+            {
+                return (_tauxAnnuel / 12);
+            }
+            else if (_periodicite == "Bimestrielle")
+            {
+                return (_tauxAnnuel / 6);
+            }
+            else if (_periodicite == "Trimestrielle")
+            {
+                return (_tauxAnnuel / 4);
+            }
+            else if (_periodicite == "Semestrielle")
+            {
+                return (_tauxAnnuel / 2);
+            }
+            else 
+            {
+                return _tauxAnnuel;
+            }
+        }
+    }
+}
