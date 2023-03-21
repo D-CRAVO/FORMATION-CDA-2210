710cebb4342dee9b3447b20e4ebfbac5ec96354a
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-03-21 11:40:23 +0100

    Update Emprunts7.1
    Mise en place d'une enum pour la périodicité

---
+++ 104_Winform/02 Exercices/107_Emprunts/Emprunts7.1/WinFormsEmprunts/WinFormsEmprunts/FormAlors.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsEmprunts
+{
+    public partial class FormAlors : Form
+    {
+        public FormAlors()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Bouton qui permet au formateur d'indiquer que l'exercice est presque juste
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonOui_Click(object sender, EventArgs e)
+        {
+            FormHourra monHourra = new FormHourra();
+            monHourra.ShowDialog();
+        }
+
+        /// <summary>
+        /// Bouton qui permet au formateur d'indiquer qu'il reste encore du travail
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonNon_Click(object sender, EventArgs e)
+        {
+            FormMince monMince = new FormMince();
+            monMince.ShowDialog();
+        }
+    }
+}
---
+++ 104_Winform/02 Exercices/107_Emprunts/Emprunts7.1/WinFormsEmprunts/WinFormsEmprunts/FormMince.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsEmprunts
+{
+    public partial class FormMince : Form
+    {
+        Random random = new Random();
+        int temps;
+        public FormMince()
+        {
+            temps = 15;
+            InitializeComponent();
+            timerMince.Start();
+        }
+
+        private void timerMince_Tick(object sender, EventArgs e)
+        {
+            temps--;
+            if (temps <= 0)
+            {
+                timerMince.Stop();
+                Close();
+            }
+
+        }
+
+        private void FormMince_Load(object sender, EventArgs e)
+        {
+            var image = imageListMince.Images;
+            foreach(var img in image)
+            {
+                random = new Random();
+                pictureBoxMince.Image = imageListMince.Images[random.Next(0, 1)];
+            }
+        }
+    }
+}
