63310c1a1934cd364e4d7c353d643c537b5eb2b5
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-03-21 14:31:02 +0100

    Update WinFormAdditionneur

---
+++ 104_Winform/02 Exercices/101_Additionneur/WinFormsAdditionneur/ClassLibraryAddition/Addition.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryAddition
+{
+    public class Addition
+    {
+        /// <summary>
+        /// Liste contenant l'ensemble des chiffres saisis
+        /// </summary>
+        private List<int> numbers;
+
+        /// <summary>
+        /// Constructeur par défaut
+        /// </summary>
+        public Addition()
+        {
+            numbers = new List<int>();
+        }
+
+        /// <summary>
+        /// Enregistre les valeurs passées en paramètre dans la liste numbers
+        /// </summary>
+        /// <param name="nb"></param>
+        public void enregistrer(int nb)
+        {
+            numbers.Add(nb);
+        }
+
+        /// <summary>
+        /// Additionne l'ensemble des valeurs de la liste numbers
+        /// </summary>
+        /// <returns>La somme obtenue</returns>
+        public int getResultat()
+        {
+            int somme = 0;
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                somme += numbers[i];
+            }
+            return somme;
+        }
+    }
+}
