8f6871e02fba7d1a573db6b74c2c1a6969c11e5b
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-03-23 07:47:23 +0100

    Create WFAdditionneur
    Création de l'exercice

---
+++ 104_Winform/02 Exercices/000_Revision/11_Additionneur/WFAdditionneur/CLAdditionneur/Calcul.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLAdditionneur
+{
+    internal class Calcul
+    {
+        private List<int>? valeurs;
+        private int somme;
+
+        public int addition()
+        {
+            somme = 0;
+            for (int i = 0; i < valeurs.Count; i++)
+            {
+                somme += valeurs[i];
+            }
+            return somme;
+        }
+
+        public void enregistrerValeur(int _nb)
+        {
+            valeurs.Add(_nb);
+        }
+
+        public void initialiserValeurs()
+        {
+            somme = 0;
+            valeurs = new List<int>();
+        }
+    }
+}
---
+++ 104_Winform/02 Exercices/000_Revision/11_Additionneur/WFAdditionneur/WFAdditionneur/FormAdditionneur.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using CLCalcul;
+using System.Net.NetworkInformation;
+
+namespace WFAdditionneur
+{
+    public partial class FormAdditionneur : Form
+    {
+        Calcul? monAddition;
+        int nb;
+        string str;
+
+        public FormAdditionneur()
+        {
+            InitializeComponent();
+            monAddition = new Calcul(); 
+            affichage(string.Empty);
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            nb = int.Parse((string)button.Tag);
+            monAddition.enregistrerValeur(nb);
+            affichage($"{nb}+");
+        }
+
+        private void buttonVider_Click(object sender, EventArgs e)
+        {
+            monAddition.initialiserValeurs();
+            textBoxAffichage.Clear();
+        }
+
+        private void buttonCalculer_Click(object sender, EventArgs e)
+        {
+            affichage($" = {monAddition.addition()}+");
+        }
+
+        private void affichage(string _str)
+        {
+            textBoxAffichage.Text += _str;
+        }
+    }
+}
