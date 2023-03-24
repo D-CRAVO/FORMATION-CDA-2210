b254e5ce654a04092c6a2c36417d53bcbf46af5a
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-03-24 08:38:27 +0100

    Create WFCheckBoxRadioButton
    Création de la FormCheckBoxRadioButton

---
+++ 104_Winform/02 Exercices/000_Revision/WFCheckBoxRadioButton/WFCheckBoxRadioButton/FormCheckBoxRadioButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFCheckBoxRadioButton
+{
+    public partial class FormCheckBoxRadioButton : Form
+    {
+        public FormCheckBoxRadioButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
