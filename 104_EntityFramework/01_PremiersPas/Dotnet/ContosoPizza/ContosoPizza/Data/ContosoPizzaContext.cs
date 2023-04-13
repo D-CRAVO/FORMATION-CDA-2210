3e8693735f456efe04056c36e5fbf8d8df8872d8
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-04-13 16:37:22 +0200

    Create ContosoPizza
    Création, mise à jour, et lecture base de données via EntityFramework

---
+++ 104_EntityFramework/01_PremiersPas/Dotnet/ContosoPizza/ContosoPizza/Data/ContosoPizzaContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContosoPizza.Models;
+
+namespace ContosoPizza.Data
+{
+    internal class ContosoPizzaContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Order> Orders { get; set; } = null!;   
+        public DbSet<Product> Products { get; set; } = null!;
+        public DbSet<OrderDetail> OrderDetails { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+    }
+}
---
+++ 104_EntityFramework/01_PremiersPas/Dotnet/ContosoPizza/ContosoPizza/Program.cs
@@ -0,0 +1,104 @@
+﻿using ContosoPizza.Data;
+using ContosoPizza.Models;
+
+using ContosoPizzaContext context = new ContosoPizzaContext();
+
+#region Insertion
+//Product veggieSpecial = new Product()
+//{
+//    Name = "Veggie Special Pizza"
+//    ,Price = 9.99M
+//};
+//context.Products.Add(veggieSpecial);
+
+//Product deluxeMeat = new Product()
+//{
+//    Name = "Deluxe Meat Pizza"
+//    ,
+//    Price = 12.99M
+//};
+//context.Add(deluxeMeat);
+
+//context.SaveChanges();
+#endregion
+
+#region Lecture version 1
+//var products = context.Products
+//                    .Where(p => p.Price > 10.00M)
+//                    .OrderBy(p => p.Name);
+
+//foreach (Product p in products)
+//{
+//    Console.WriteLine($"Id:     {p.Id}");
+//    Console.WriteLine($"Name:   {p.Name}");
+//    Console.WriteLine($"Price:  {p.Price}");
+//    Console.WriteLine(new string('-', 20));
+//}
+#endregion
+
+#region Lecture version 2
+//var products = from product in context.Products
+//               where product.Price > 10.00M
+//               orderby product.Name
+//               select product;
+
+//foreach (Product p in products)
+//{
+//    Console.WriteLine($"Id:     {p.Id}");
+//    Console.WriteLine($"Name:   {p.Name}");
+//    Console.WriteLine($"Price:  {p.Price}");
+//    Console.WriteLine(new string('-', 20));
+//}
+#endregion
+
+#region Modification et lecture
+//var veggieSpecial = context.Products
+//                        .Where (p => p.Name == "Veggie Special Pizza")
+//                        .FirstOrDefault ();
+
+//if(veggieSpecial is Product)
+//{
+//    veggieSpecial.Price = 10.99M;
+//}
+
+//context.SaveChanges();
+
+//var products = from product in context.Products
+//               where product.Price > 10.00M
+//               orderby product.Name
+//               select product;
+
+//foreach (Product p in products)
+//{
+//    Console.WriteLine($"Id:     {p.Id}");
+//    Console.WriteLine($"Name:   {p.Name}");
+//    Console.WriteLine($"Price:  {p.Price}");
+//    Console.WriteLine(new string('-', 20));
+//}
+#endregion
+
+#region Suppression et lecture
+var veggieSpecial = context.Products
+                        .Where(p => p.Name == "Veggie Special Pizza")
+                        .FirstOrDefault();
+
+if (veggieSpecial is Product)
+{
+    context.Remove(veggieSpecial);
+}
+
+context.SaveChanges();
+
+var products = from product in context.Products
+               where product.Price > 10.00M
+               orderby product.Name
+               select product;
+
+foreach (Product p in products)
+{
+    Console.WriteLine($"Id:     {p.Id}");
+    Console.WriteLine($"Name:   {p.Name}");
+    Console.WriteLine($"Price:  {p.Price}");
+    Console.WriteLine(new string('-', 20));
+}
+#endregion
