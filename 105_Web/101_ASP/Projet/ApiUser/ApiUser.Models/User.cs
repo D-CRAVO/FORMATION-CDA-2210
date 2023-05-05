31a9ccc65b37eb46b7dcc521c5b7e91b0abcfc8b
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-05-05 11:54:53 +0200

    Create ApiUser

---
+++ 105_Web/101_ASP/Projet/ApiUser/ApiUser.Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiUser.Models
+{
+    [Table("Users")]
+    public class User : Model
+    {
+        [Required(ErrorMessage ="")]
+        [MaxLength(16, ErrorMessage ="")]
+        [StringLength(16)]
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
