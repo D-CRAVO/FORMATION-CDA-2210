fc5c91388f014c3018655c50dfd91aa8de76e6fb
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-05-05 11:54:32 +0200

    Create Movies

---
+++ 105_Web/101_ASP/Personnel/Movies/Movies/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+        [Required]
+        public DateTime Year { get; set; }
+        [Required]
+        public int Length { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Subject { get; set; }
+        public string Actor { get; set; }
+        public string 
+    }
+}
