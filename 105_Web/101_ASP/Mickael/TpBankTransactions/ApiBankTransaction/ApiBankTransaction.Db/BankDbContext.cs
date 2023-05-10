93fe4a2985e735e81800c1036dd8444326f7fc3f
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-05-10 20:02:27 +0200

    Read ApiUser
    Relecture exercice

---
+++ 105_Web/101_ASP/Mickael/TpBankTransactions/ApiBankTransaction/ApiBankTransaction.Db/BankDbContext.cs
@@ -0,0 +1,17 @@
+﻿using ApiBankTransaction.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ApiBankTransaction.Db
+{
+    public class BankDbContext : DbContext
+    {
+        public DbSet<BankTransaction> BankTransactions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=tp_banks");
+        }
+    }
+}
--- 105_Web/101_ASP/Mickael/TpBankTransactions/ApiBankTransaction/ApiBankTransaction.Models/BankTransaction.cs
+++ 105_Web/101_ASP/Mickael/TpBankTransactions/ApiBankTransaction/ApiBankTransaction.Models/BankTransaction.cs
@@ -1,7 +1,39 @@
-﻿namespace ApiBankTransaction.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using CLControls;
+
+namespace ApiBankTransaction.Models
 {
+    [Table("Transactions")]
     public class BankTransaction
     {
+        [Key][Column ("transaction_id")]
+        public int TransactionId { get; private set; }
+        [Required][Column("transaction_date")]
+        
+        public DateTime TransactionDate { get; private set; }
 
+        [Required][Column("transaction_from")]
+        [RegularExpression(Control.AccountNumber)]
+        public uint TransactionFrom { get; private set; }
+
+        [Required][Column("transaction_to")]
+        [RegularExpression(Control.AccountNumber)]
+        public uint TransactionTo { get; private set; }
+
+        [Required][Column("transaction_amount")]
+        public float TransactionAmount { get; private set; }
+
+        public BankTransaction(DateTime _transactionDate, uint _transactionFrom, uint _transactionTo, float _transactionAmount)
+        {
+            TransactionDate = _transactionDate;
+            TransactionFrom = _transactionFrom;
+            TransactionTo = _transactionTo;
+            if (Control.Amount(_transactionAmount))
+            {
+                TransactionAmount = _transactionAmount;
+            }
+            
+        }
     }
 }
