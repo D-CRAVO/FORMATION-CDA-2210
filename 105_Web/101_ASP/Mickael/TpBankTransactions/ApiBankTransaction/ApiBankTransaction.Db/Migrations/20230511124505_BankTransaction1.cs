97fcef118f75f1fc016473152acf6a08c838e481
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-05-11 17:01:49 +0200

    Update TpBankTransaction
    Génération de la base de données

---
+++ 105_Web/101_ASP/Mickael/TpBankTransactions/ApiBankTransaction/ApiBankTransaction.Db/Migrations/20230511124505_BankTransaction1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiBankTransaction.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class BankTransaction1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    transaction_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    transaction_date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    transaction_from = table.Column<long>(type: "bigint", nullable: false),
+                    transaction_to = table.Column<long>(type: "bigint", nullable: false),
+                    transaction_amount = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.transaction_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
--- 105_Web/101_ASP/Mickael/TpBankTransactions/ApiBankTransaction/ApiBankTransaction.Models/BankTransaction.cs
+++ 105_Web/101_ASP/Mickael/TpBankTransactions/ApiBankTransaction/ApiBankTransaction.Models/BankTransaction.cs
@@ -1,39 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using CLControls;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiBankTransaction.Models
 {
     [Table("Transactions")]
-    public class BankTransaction
+    public class BankTransaction : Model
     {
-        [Key][Column ("transaction_id")]
-        public int TransactionId { get; private set; }
         [Required][Column("transaction_date")]
-        
-        public DateTime TransactionDate { get; private set; }
+        public DateTime TransactionDate { get; set; }
 
         [Required][Column("transaction_from")]
         [RegularExpression(Control.AccountNumber)]
-        public uint TransactionFrom { get; private set; }
+        public uint TransactionFrom { get; set; }
 
         [Required][Column("transaction_to")]
         [RegularExpression(Control.AccountNumber)]
-        public uint TransactionTo { get; private set; }
+        public uint TransactionTo { get; set; }
 
-        [Required][Column("transaction_amount")]
-        public float TransactionAmount { get; private set; }
+        [Required]
+        [Column("transaction_amount")]
+        [Precision(7, 2)]
+        [Range(0.0d,99000.0d)]
+        public decimal TransactionAmount { get; set; }
+        
 
-        public BankTransaction(DateTime _transactionDate, uint _transactionFrom, uint _transactionTo, float _transactionAmount)
-        {
-            TransactionDate = _transactionDate;
-            TransactionFrom = _transactionFrom;
-            TransactionTo = _transactionTo;
-            if (Control.Amount(_transactionAmount))
-            {
-                TransactionAmount = _transactionAmount;
-            }
-            
-        }
+        
     }
 }
