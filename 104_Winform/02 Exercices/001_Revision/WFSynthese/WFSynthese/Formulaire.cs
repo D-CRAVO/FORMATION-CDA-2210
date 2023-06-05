3ab71c830a142c5d88d72e2921c8a10ee30bc9a7
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-06-05 10:39:50 +0200

    Sauvegarde rapide

--- 104_Winform/02 Exercices/001_Revision/WFSynthese/WFSynthese/Formulaire.cs
+++ 104_Winform/02 Exercices/001_Revision/WFSynthese/WFSynthese/Formulaire.cs
@@ -13,26 +13,56 @@ namespace WFSynthese
 {
     public partial class Formulaire : Form
     {
+        Emprunt empruntEtude;
         public Formulaire()
         {
             InitializeComponent();
+            Emprunt empruntDepart = new Emprunt();
+            Affichage(empruntDepart);
         }
 
         public Formulaire(Emprunt _emprunt)
         {
             InitializeComponent();
-            InitialisationFormulaire(_emprunt);
+            empruntEtude = new Emprunt(_emprunt);
+            Affichage(empruntEtude);
         }
 
-        private void InitialisationFormulaire(Emprunt _emprunt)
+        private void Affichage(Emprunt _emprunt)
         {
-
+            textBoxNom.Text = _emprunt.Nom;
+            textBoxCapital.Text = _emprunt.Capital.ToString();
+            hScrollBarDuree.Value = (int)_emprunt.Duree;
+            listBoxPeriodicite.SelectedItem = _emprunt.Periodicite;
+            CheckTaux(_emprunt);
+            //textBoxNbRemboursements.Text = CalculNbRemboursements(_emprunt);
+            //textBoxMontantRemboursements.Text = CalculMontantRemboursements(_emprunt);
         }
 
-        private void Affichage()
+        private void CheckTaux(Emprunt _emprunt)
         {
-
+            if (_emprunt.TauxAnnuel == 0.07f)
+            {
+                radioButton7.Checked = true;
+            }
+            else if (_emprunt.TauxAnnuel == 0.08f)
+            {
+                radioButton8.Checked = true;
+            }
+            else if (_emprunt.TauxAnnuel == 0.09f)
+            {
+                radioButton9.Checked = true;
+            }
         }
+
+        private void ChoosePeriodicite()
+        {
+            if (listBoxPeriodicite.SelectedIndex == 0)
+            {
+                
+            }
+        }
+
 
         private void listBoxPeriodicite_SelectedIndexChanged(object sender, EventArgs e)
         {
