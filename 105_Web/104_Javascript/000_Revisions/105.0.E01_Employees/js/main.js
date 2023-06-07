a68dd9f9e25ce0b2fd21e7014bb32ab6961c3dba
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-06-07 20:05:26 +0200

    Update main.js

--- 105_Web/104_Javascript/000_Revisions/105.0.E01_Employees/js/main.js
+++ 105_Web/104_Javascript/000_Revisions/105.0.E01_Employees/js/main.js
@@ -1,47 +1,74 @@
- import {Employees} from "./Employees.js"
+import {Employees} from "./Employees.js"
  
- const employees = new Employees("/json/employees.json");
- await employees.getEmployees();
+const employees = new Employees("/json/employees.json");
+await employees.getEmployees();
 
- tbody = document.getElementById('tbody')
+tbody = document.getElementById('tbody')
 
- function createEid(){
-    for(let employee of employees.collection){
-        let row = document.createElement('tr');
-        row.setAttribute('id', `row${employee.id}`)
-        let cell = document.createElement('td');
-        row.appendChild(cell);
-        tbody.appendChild(row);
-        cell.textContent = employee.id;
-    }
- }
+function createEid(){
+   for(let employee of employees.collection){
+      let row = document.createElement('tr');
+      row.setAttribute('id', `row${employee.id}`)
+      let cell = document.createElement('td');
+      row.appendChild(cell);
+      tbody.appendChild(row);
+      cell.textContent = employee.id;
+   }
+}
 
- function createFullName(){
-    for(let employee of employees.collection){
-        let row = document.getElementById(`row${employee.id}`);
-        let cell = document.createElement('td');
-        cell.textContent = employee.employee_name;
-        row.appendChild(cell);
-    }
- }
+function createFullName(){
+   for(let employee of employees.collection){
+      let row = document.getElementById(`row${employee.id}`);
+      let cell = document.createElement('td');
+      cell.textContent = employee.employee_name;
+      row.appendChild(cell);
+   }
+}
 
- function createEmail(){
-    for(let employee)
- }
+function createEmail(){
+   for(let employee of employees.collection){
+      let row = document.getElementById(`row${employee.id}`);
+      let cell = document.createElement('td');
+      let fullname = employee.employee_name.split(' ');
+      cell.textContent = `${fullname[0].toLowerCase().substring(0,1)}` + `${fullname[1].toLowerCase()}`;
+      row.appendChild(cell)
+   }
+}
 
- function createRowCell(data){
-    let row = document.createElement('tr');
-    let cell = document.createElement('td');
-    row.appendChild(cell);
-    tbody.appendChild(row);
-    cell.textContent = data;
- }
+function createMonthlySalary(){
+   for(let employee of employees.collection){
+      let row = document.getElementById(`row${employee.id}`);
+      let cell = document.createElement('td');
+      cell.textContent = (employee.employee_salary / 12).toFixed(2);
+      row.appendChild(cell);
+   }
+}
 
- function createCell(data){
-    let cell = document.createElement('td');
-    row.appendChild(cell);
-    cell.textContent = data;
- }
+function yearOfBirth(){
+   for(let employee of employees.collection){
+      let row = document.getElementById(`row${employee.id}`)
+      let cell = document.createElement('td');
+      cell.textContent = (new Date()).getFullYear()- employee.employee_age
+      row.appendChild(cell);
+   }
+}
 
- createEid();
- createFullName();
+function createRowCell(data){
+   let row = document.createElement('tr');
+   let cell = document.createElement('td');
+   row.appendChild(cell);
+   tbody.appendChild(row);
+   cell.textContent = data;
+}
+
+function createCell(data){
+   let cell = document.createElement('td');
+   row.appendChild(cell);
+   cell.textContent = data;
+}
+
+createEid();
+createFullName();
+createEmail();
+createMonthlySalary();
+yearOfBirth()
