422fd7d7c4b7571b8b87bbc760726c572a298d3b
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-06-09 16:02:59 +0200

    Update main.js

--- 105_Web/104_Javascript/105.1_Employees/js/main.js
+++ 105_Web/104_Javascript/105.1_Employees/js/main.js
@@ -1,3 +1,18 @@
-import {Employees} from "./Employees.js"
+import {Employee} from "./Employee.js";
+import {Employees} from "./Employees.js";
 
-Employees.getEmployees();
+let employees = new Employees ("/json/employees.json");
+employees.getEmployees();
+
+const tbody = document.getElementById("tbody");
+
+console.log(employees.collection);
+
+function createTable(){
+    for(let employee of employees.collection){
+        console.log(employee.employee_name)
+    }
+}
+console.log ("toto");
+
+createTable();
