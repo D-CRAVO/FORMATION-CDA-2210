a9dc439f48566aff03ac61f509a9f34cd79b8209
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-06-12 08:30:02 +0200

    Update Employees.js

--- 105_Web/104_Javascript/105.1_Employees/js/Employees.js
+++ 105_Web/104_Javascript/105.1_Employees/js/Employees.js
@@ -14,6 +14,36 @@ class Employees{
             this.collection.push(employee);
         }
     }
+
+    calculateNbEmployees(){
+        return this.collection.length;
+    }
+
+    calculateTotalSalary(){
+        totalSalary = 0;
+        for(let emp of this.collection){
+            let employee = new Employee(emp);
+            totalSalary += parseFloat(employee.calculateMonthlySalary());
+        }
+        return (totalSalary.toFixed(2));
+    }
+
+    duplicateEmployee(employeeId){
+        let currentEmployee = this.collection.find(emp => emp.id == employeeId)
+        let newEmployee = new Employee(currentEmployee);
+        let maxId = (this.collection.sort((a,b)=> a.id - b.id))[this.collection.length-1].id;
+        newEmployee.id = ++maxId;
+        this.collection.push(newEmployee);
+    }
+
+    deleteEmployee(employeeId){
+        console.log(employeeId);
+        this.collection = this.collection.filter(emp => emp.id !== employeeId);
+        console.log(this.collection);
+
+    }
+
+    
 }
 
 
