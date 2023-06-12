03e78fcd636fb4c735b11f96a68b354ec605ca25
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-06-12 08:30:00 +0200

    Update Employee.js

--- 105_Web/104_Javascript/105.1_Employees/js/Employee.js
+++ 105_Web/104_Javascript/105.1_Employees/js/Employee.js
@@ -10,6 +10,19 @@ class Employee{
     getKeys(){
         Object.keys(this);
     }
+
+    calculateEmail(){
+        let fullName = this.employee_name.split(" ");
+        return `${fullName[0].toLowerCase().substring(0,1)}.${fullName[1].toLowerCase()}`
+    }
+
+    calculateMonthlySalary(){
+        return (this.employee_salary/12).toFixed(2);
+    }
+
+    calculateYearOfBirth(){
+        return new Date().getFullYear() - this.employee_age;
+    }
 }
 
 export {Employee};
