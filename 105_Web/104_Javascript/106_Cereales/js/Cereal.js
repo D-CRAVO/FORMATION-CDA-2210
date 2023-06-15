2948aaf6f087a93733ea9b296b72b53181f4a6aa
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-06-15 21:29:12 +0200

    Update Cereal.js

--- 105_Web/104_Javascript/106_Cereales/js/Cereal.js
+++ 105_Web/104_Javascript/106_Cereales/js/Cereal.js
@@ -10,6 +10,27 @@ class Cereal{
     getKeys(){
         return Object.keys(this);
     }
+
+    CalculateNutriScore(){
+        let data = this.rating
+        let result = ""
+        if(data >= 80){
+            result = 'a';
+        }else if (data>=70){
+            result = 'b';
+        }else if(data>=55){
+            result = 'c';
+        }else if(data>=35){
+            result = 'd';
+        }else{
+            result = 'e';
+        }
+        return result;
+
+
+    }
+
+    
 }
 
 export {Cereal};
