76461a22e4468077d80e4272be1a3bb7c73dc042
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-06-23 11:55:18 +0200

    sauvegarde de fin de matinée

--- 105_Web/105_React/500_DemoNodeMickael/demo-react/pokemon-creator/pages/pokemon/liste.js
+++ 105_Web/105_React/500_DemoNodeMickael/demo-react/pokemon-creator/pages/pokemon/liste.js
@@ -1,5 +1,23 @@
-export default function PokemonListe(){
-    return (
-        <h1>Liste des Pokemons</h1>
-    )
+import React from 'react';
+import Image from 'next/image';
+
+export default class PokemonListe extends React.Component
+{
+    render()
+    {
+        return (
+            <div>
+                <h1>Liste des Pokemons</h1>
+                <Image src="/images/Pikachu.png" width={1425} height={1775} alt="Pikachu"></Image>
+            </div>
+        );
+    }
 }
+
+
+
+// export default function PokemonListe(){
+//     return (
+//         <h1>Liste des Pokemons</h1>
+//     );
+// }
