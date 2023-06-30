72b21477bdf433b2b7182c4fd349b17927d261bf
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-06-30 11:55:49 +0200

    Sauvegarde de fin de matinée

---
+++ 105_Web/106_Accessibilite/ProjetHandiA11y/js/accordeon.js
@@ -0,0 +1,4 @@
+//on récupère les éléments pour l'accordéon
+const toggleButton = document.getElementById('toggle-description');
+const descriptionDiv = document.getElementById('image-description');
+
