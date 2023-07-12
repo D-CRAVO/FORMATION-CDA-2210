da9ce28232439d6691078ea7a8bf81eb6cb543b1
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-07-12 17:01:23 +0200

    Sauvegarde de fin d'après-midi

---
+++ 106_DesignPattern/Cours/DesignPatternComposite/DesignPatternComposite/DesignPatternComposite/Binaire.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternComposite
+{
+    abstract class Binaire : Expression
+    {
+        private Expression op1;
+        private Expression op2;
+        public Binaire(Expression _op1, Expression _op2)
+        {
+            this.op1 = _op1;
+            this.op2 = _op2;
+        }
+    }
+}
