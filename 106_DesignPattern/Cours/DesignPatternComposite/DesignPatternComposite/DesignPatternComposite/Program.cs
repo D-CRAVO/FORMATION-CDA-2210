dcf630ccd313c336b0b01d84c60a2c4c956c76c5
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-07-13 11:56:15 +0200

    Sauvegarde de fin de matinée

--- 106_DesignPattern/Cours/DesignPatternComposite/DesignPatternComposite/DesignPatternComposite/Program.cs
+++ 106_DesignPattern/Cours/DesignPatternComposite/DesignPatternComposite/DesignPatternComposite/Program.cs
@@ -4,7 +4,7 @@ Expression exp1 = new Addition(new Nombre(33), new Nombre(33));
 double resultat1 = exp1.Evalue();
 Console.WriteLine(resultat1);
 
-Expression exp2 = new Addition(new Nombre(33), new Addition (new Nombre(33), new Nombre(11)));
+Expression exp2 = new Addition(new Nombre(33), new Addition (new Addition(new Nombre(22), new Nombre(11)), new Nombre(11)));
 double resultat2 = exp2.Evalue();
 Console.WriteLine(resultat2);
 
