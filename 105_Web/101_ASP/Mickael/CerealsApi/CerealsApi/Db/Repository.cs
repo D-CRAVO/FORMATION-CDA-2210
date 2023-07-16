8ff166d66f27e42b554850705d7ea9497b167032
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-07-16 19:23:06 +0200

    Relecture exercices

---
+++ 105_Web/101_ASP/Mickael/CerealsApi/CerealsApi/Db/Repository.cs
@@ -0,0 +1,20 @@
+﻿using CerealsApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace CerealsApi.Db
+{
+    public class Repository<T> where T : Model
+    {
+        private DbContext ctx;
+        public Repository(DbContext context) 
+        {
+            ctx = context;            
+        }
+
+        public T Get()
+        {
+            return ctx.
+        }
+    }
+}
