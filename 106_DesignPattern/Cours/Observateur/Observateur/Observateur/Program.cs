5649f41db00e0a05e6894675f0bb05c1a77ba88d
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-07-24 20:42:19 +0200

    Relecture code

--- 106_DesignPattern/Cours/Observateur/Observateur/Observateur/Program.cs
+++ 106_DesignPattern/Cours/Observateur/Observateur/Observateur/Program.cs
@@ -3,6 +3,6 @@ using System.Resources;
 
 
 Navigateur n = new Navigateur();
-Satellite s = new Satellite(10, "essai");
+Satellite s = new Satellite(10, "41°24'12.2\"N 2°10'26.5\"E");
 s.AjouterObservateur(n);
 s.NotifierObservateur();
