ef600eb5c824da361b157c145d980c1dcbaad373
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-07-28 11:53:03 +0200

    Create Evaluation Sogeti

---
+++ 106_DesignPattern/001_Tests/EvaluationSogeti/ElementsChimiques/CLElementsChimiques/Atome.cs
@@ -0,0 +1,10 @@
+﻿namespace CLElementsChimiques
+{
+    public class Atome : ElementChimique
+    {
+        public override void Associer()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
