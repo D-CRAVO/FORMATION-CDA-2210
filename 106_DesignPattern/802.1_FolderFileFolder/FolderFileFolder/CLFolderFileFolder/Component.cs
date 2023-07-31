79d8db411f921f3ddc2ec730d2455ef612be3726
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-07-31 07:14:35 +0200

    Create FolderFileFolder

---
+++ 106_DesignPattern/802.1_FolderFileFolder/FolderFileFolder/CLFolderFileFolder/Component.cs
@@ -0,0 +1,17 @@
+﻿namespace CLFolderFileFolder
+{
+    public abstract class Component
+    {
+        private string name;
+
+        public string Name { get { return name; } }
+
+        public Component(string name)
+        {
+            this.name = name;
+        }
+
+        public abstract void Display();
+
+    }
+}
---
+++ 106_DesignPattern/802.1_FolderFileFolder/FolderFileFolder/CLFolderFileFolder/File.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLFolderFileFolder
+{
+    public class File : Component
+    {
+        public File(string name) : base(name)
+        {
+            
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"Fichier {Name}");
+        }
+    }
+}
