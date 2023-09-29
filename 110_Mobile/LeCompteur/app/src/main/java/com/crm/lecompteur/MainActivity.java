504e519e2e0633ead9d147dfe8422052ce0faba1
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-09-29 11:53:20 +0200

    Update LeCompteur

--- 110_Mobile/LeCompteur/app/src/main/java/com/crm/lecompteur/MainActivity.java
+++ 110_Mobile/LeCompteur/app/src/main/java/com/crm/lecompteur/MainActivity.java
@@ -2,19 +2,42 @@ package com.crm.lecompteur;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textView;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = (TextView)findViewById(R.id.compteur);
     }
 
-    public String incrementer(){
+    public void incrementer(){
+        int t = Integer.parseInt(String.valueOf(textView));
+        t++;
+    t.
+    }
 
-        return
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button inc = (Button) findViewById(R.id.inc);
+        inc.setOnClickListener(new OnClickListener());
+
+        public void onClick(View v) {
+            EditText mValue = (EditText) findViewById(R.id.compteur);
+            int val=Integer.parseInt(mValue.getText().toString());
+            --val;
+            mValue.setText(Integer.toString(val));
+        }
     }
 }
--- 110_Mobile/MaPremiereApplication/app/src/main/java/com/crm/mapremiereapplication/MaPremiereActivity.java
+++ 110_Mobile/MaPremiereApplication/app/src/main/java/com/crm/mapremiereapplication/MaPremiereActivity.java
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MaPremiereActivity extends AppCompatActivity {
 
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ma_premiere);
+        t = (TextView)findViewById()
     }
 
 
