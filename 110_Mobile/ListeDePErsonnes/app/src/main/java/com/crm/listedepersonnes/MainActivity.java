2d57789cb4b399a478595619c3c602d2791fa934
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-10-03 08:42:38 +0200

    Relecture exercice

--- 110_Mobile/ListeDePErsonnes/app/src/main/java/com/crm/listedepersonnes/MainActivity.java
+++ 110_Mobile/ListeDePErsonnes/app/src/main/java/com/crm/listedepersonnes/MainActivity.java
@@ -1,20 +1,24 @@
 package com.crm.listedepersonnes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.*;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] tab = new String[]{"membre1", "membre2", "membre3", "membre4", "membre5"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter arrayadp = new ArrayAdapter(this, android.R.layout.simple_list_item_1, tab);
+
     }
 
 /*
-    String[] tab = new String[]{"membre1", "membre2", "membre3", "membre4", "membre5"};
-    ArrayAdapter arrayadp = new ArrayAdapter(this, android.R.layout.simple_list_item_1, tab);
+
 */
 }
