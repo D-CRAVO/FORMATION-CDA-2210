ab9fdbbe22869a9cba099d9e0af1fb5abc01bfd9
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-10-05 08:55:41 +0200

    Sperquiz Android studio

--- 110_Mobile/SuperQuiz/app/src/main/java/com/example/superquiz/WelcomeFragment.java
+++ 110_Mobile/SuperQuiz/app/src/main/java/com/example/superquiz/WelcomeFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,6 +41,34 @@ public class WelcomeFragment extends Fragment {
     @Override
     public void onViewCreated( View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.playButton.setEnabled(true);
+        binding.playButton.setEnabled(false);
+
+        // Vérifier si la saisie de l'utilisateur n'est pas vide et dans ce cas autorisé le clic
+        binding.usernameEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                boolean isEmpty = s.toString().isEmpty();
+                binding.playButton.setEnabled(!isEmpty);
+            }
+        });
+
+        // Détecter le clic sur le bouton Let's Play pour démarrer le quiz
+        binding.playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Naviguez vers le fragment quiz
+                Log.d("David", "Click");
+            }
+        });
     }
 }
