3754157e98ba0f7b93877c48f097ff51763e03f4
CRAVO David <104896215+D-CRAVO@users.noreply.github.com>
2023-10-10 12:35:54 +0200

    Révisions

---
+++ 109_Tests/RevisionBarnabe/ex37_barnabe/exercice_3-7_barnabe/CalculNbMagasins.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_3_7_barnabe
+{
+    public class CalculNbMagasins
+    {
+        private int magasins_visites;
+        public CalculNbMagasins()
+        {
+            magasins_visites = 0;
+        }
+
+
+        public int NbMagasinRealisePArBarnabe(float sommeDansLePorteMonnaieDeBarnabe)
+        {
+
+            while (sommeDansLePorteMonnaieDeBarnabe > 2)
+            {
+                sommeDansLePorteMonnaieDeBarnabe = sommeDansLePorteMonnaieDeBarnabe - (sommeDansLePorteMonnaieDeBarnabe / 2 + 1);
+                magasins_visites++;
+            }
+            return magasins_visites + 1;
+        }
+    } 
+}
---
+++ 109_Tests/RevisionBarnabe/ex37_barnabe/exercice_3-7_barnabe/Program.cs
@@ -0,0 +1,42 @@
+﻿using exercice_3_7_barnabe;
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ex37_barnabe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCICE 3.7 BARNABE FAIT SES COURSES
+
+            // VARIABLES
+
+            float argent = 0;
+            int magasins_visites = 0;
+
+            CalculNbMagasins calculNbMagasins = new CalculNbMagasins();
+
+            // DEBUT PROGRAMME
+
+            Console.Write("Veuillez entrer la somme de départ : ");
+            argent = float.Parse(Console.ReadLine());
+            magasins_visites = calculNbMagasins.NbMagasinRealisePArBarnabe(argent);
+            // On affiche magasins_visites + 1 pour tenir compte du
+            // dernier magasin où il dépense le solde.
+            Console.WriteLine("Barbabé a visité " + (magasins_visites + 1) + " magasins.");
+
+            // FIN PROGRAMME
+
+            // DEBUT FONCTION
+
+            // Je vous demande d'établir les tests unitaires pour vérifier que ma fonction
+            // int NbMagasinRealisePArBarnabe(decimal sommeDansLePorteMonnaieDeBarnabe)
+            
+            
+        }
+
+        // FIN FONCTION
+    }
+    
+}
